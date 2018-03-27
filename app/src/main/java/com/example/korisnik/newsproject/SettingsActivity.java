77c7c4929eb7ad2881e9bb769e1c525d1216e52a
Milos Pavicic <milospavicic@yahoo.com>
2018-03-28 00:39:24 +0200

    Preferences.xml and small changes on layouts

--- app/src/main/java/com/example/korisnik/newsproject/SettingsActivity.java
+++ app/src/main/java/com/example/korisnik/newsproject/SettingsActivity.java
@@ -1,13 +1,14 @@
 package com.example.korisnik.newsproject;
 
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
